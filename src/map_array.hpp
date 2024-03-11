7c6290bab2407212f1dda7ddc7cde0b7680260de
Minghe Fu <minghef@uci.edu>
2024-03-10 18:42:56 -0700

    Submission commit of HW9.

---
+++ src/map_array.hpp
@@ -0,0 +1,14 @@
+#ifndef MAP_ARRAY_HPP
+#define MAP_ARRAY_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+
+template <typename Key, typename Value>
+
+class MapArray{
+	
---
+++ src/set_list.hpp
@@ -0,0 +1,89 @@
+
+#ifndef SET_LIST_HPP
+#define SET_LIST_HPP
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <ranges>
+
+template <typename T>
+class SetList {
+	struct ListNode {
+		T data;
+		std::shared_ptr<ListNode> next;
+	};
+
+public:
+	class ListIterator {
+		public:
+			using iterator_category = std::forward_iterator_tag;
+		    using value_type = T;
+			using differece_type = std::ptrdiff_t;
+			using pointer = T*;
+			using reference = T&;
+
+			explicit ListIterator(std::shared_ptr<ListNode> ptr = nullptr) : ptr(ptr) {}
+	
+			ListIterator& operator++() {
+				if (ptr) ptr = ptr->next;
+				return *this;
+			}
+			ListIterator operator++(int) {
+				ListIterator temp = *this;
+    			++(*this);
+    			return temp;
+			}
+
+			T& operator*() const {
+				return ptr->data;
+			}
+			T* operator->() const {
+				return &(ptr->data);
+			}
+			bool operator==(const ListIterator& other) const = default;
+		private:
+			std::shared_ptr<ListNode> ptr;
+	};
+	using value_type = T;
+	using iterator = ListIterator;
+
+	SetList() = default;
+
+	template <std::ranges::input_range Rng>
+	explicit SetList(Rng&& rng) {
+		std::ranges::for_each(std::forward<Rng>(rng),std::bind_front(&SetList::insert,this));
+	}
+
+	ListIterator begin() {
+	return ListIterator(head);
+	}	
+	ListIterator end() {
+		return ListIterator(nullptr);
+	}
+	bool contains(const T& value) {
+		for (std::shared_ptr<ListNode> current = head; current != nullptr; 
+		current = current->next) {
+        if (current->data == value) {
+            return true;
+        }
+    }
+    return false;
+	}
+	ListIterator insert(T value) {
+		for (auto current = head; current != nullptr; current = current->next) {
+        if (current->data == value) {
+            return ListIterator(current);
+        }
+    }
+    auto newNode = std::make_shared<ListNode>(value, head);
+    head = newNode;
+    return ListIterator(newNode);
+}
+private:
+	std::shared_ptr<ListNode> head = nullptr;
+};
+
+#endif
+
